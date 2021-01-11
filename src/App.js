8962c8ea695b6e2d2f36b80b950673c93b91d933
saeed <sarended@gmail.com>
2021-01-11 17:37:50 +0330

    feat: react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div>hello world</div>;
-}
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// const Home = lazy(() => import())
+// const About = lazy(() => import())
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/">
+            <div>home</div>
+          </Route>
+          <Route path="/about">
+            <div>about</div>
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
 
 export default App;
